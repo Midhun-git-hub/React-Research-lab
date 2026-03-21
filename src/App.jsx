22e6ebe557eff3c26f24a9e5ca5e286c9d7947bc
Midhun-git-hub <m20787549@gmail.com>
2026-03-21 11:08:34 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import CardFlipProps from './Contents/CardFlipProps.jsx';
 import Intro from './Contents/Intro.jsx';
 

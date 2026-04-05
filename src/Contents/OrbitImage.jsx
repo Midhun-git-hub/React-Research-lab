a57d8270059cd625e379159da7ea50bbf52ad0ec
Midhun-git-hub <m20787549@gmail.com>
2026-04-05 13:43:59 +0530

    layout fix on progress

--- src/Contents/OrbitImage.jsx
+++ src/Contents/OrbitImage.jsx
@@ -4,31 +4,36 @@ import hero1 from '/images/hero.png'
 
 const OrbitImage = () => {
 
-    const images = [
-  hero1,
-  "https://picsum.photos/300/300?grayscale&random=2",
-  "https://picsum.photos/300/300?grayscale&random=3",
-  "https://picsum.photos/300/300?grayscale&random=4",
-  "https://picsum.photos/300/300?grayscale&random=5",
-  "https://picsum.photos/300/300?grayscale&random=6",
-];
+  const images = [
+    hero1,
+    "https://picsum.photos/300/300?grayscale&random=2",
+    "https://picsum.photos/300/300?grayscale&random=3",
+    "https://picsum.photos/300/300?grayscale&random=4",
+    "https://picsum.photos/300/300?grayscale&random=5",
+    "https://picsum.photos/300/300?grayscale&random=6",
+  ];
 
   return (
-    <OrbitImages
-  images={images}
-  shape="ellipse"
-  radiusX={340}
-  radiusY={80}
-  rotation={-8}
-  duration={30}
-  itemSize={80}
-  responsive={true}
-  radius={160}
-  direction="normal"
-  fill
-  showPath
-  paused={false}
-/>
+    <section className='min-h-screen w-full px-6 py-10 flex items-center justify-center'>
+      <div className='w-[700px] h-[300px] mx-auto relative overflow-hidden'>
+        <OrbitImages
+          images={images}
+          shape="ellipse"
+          radiusX={400}
+          radiusY={80}
+          rotation={-8}
+          duration={120}
+          itemSize={80}
+          responsive={true}
+          radius={160}
+          direction="normal"
+          showPath={true}
+          pathColor='rgba(0,0,0,0.1)'
+          pathWidth={2}
+          paused={false}
+        />
+      </div>
+    </section>
   )
 }
 
